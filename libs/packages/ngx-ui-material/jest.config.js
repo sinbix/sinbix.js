a96831f2a5746489fa185d1a5326de3f1a17dc34
sinbix <sinbix001@gmail.com>
2020-09-14 07:28:32 +0300

    feat: add ngx-ui-material

---
+++ libs/packages/ngx-ui-material/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'packages-ngx-ui-material',
+  preset: '../../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/packages/ngx-ui-material',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
