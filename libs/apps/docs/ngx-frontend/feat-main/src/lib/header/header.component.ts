dd43f08bf1b2a5ca30159bccd9bd091465ce1086
sinbix <sinbix001@gmail.com>
2020-09-23 03:55:04 +0300

    refactor: header styles

--- libs/apps/docs/ngx-frontend/feat-main/src/lib/header/header.component.ts
+++ libs/apps/docs/ngx-frontend/feat-main/src/lib/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'docs-main-header',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
+
+  @HostBinding('class') class = '_primary';
 
   ngOnInit(): void {}
 }
