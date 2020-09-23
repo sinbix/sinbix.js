e376601e1ad59697945e1311fcb75fa82b7ebd3e
sinbix <sinbix001@gmail.com>
2020-09-23 04:09:22 +0300

    feat: add package models

---
+++ libs/packages/models/src/lib/packages-models.spec.ts
@@ -0,0 +1,7 @@
+import { packagesModels } from './packages-models';
+
+describe('packagesModels', () => {
+  it('should work', () => {
+    expect(packagesModels()).toEqual('packages-models');
+  });
+});
---
+++ libs/packages/models/src/lib/packages-models.ts
@@ -0,0 +1,3 @@
+export function packagesModels(): string {
+  return 'packages-models';
+}
