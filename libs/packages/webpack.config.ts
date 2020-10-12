2a1ea99a38781d542db396bfa3ce1e5220c229bb
sinbix <sinbix001@gmail.com>
2020-10-12 15:13:33 +0300

    refactor: webpack config

--- libs/packages/webpack.config.ts
+++ libs/packages/webpack.config.ts
@@ -2,17 +2,9 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 
 export default (config: webpack.Configuration) => {
-  config.resolve.alias['@sinbix/ngx-ui/theming'] = path.resolve(
-    __dirname,
-    'ngx-ui/_theming'
-  );
   config.resolve.alias['@sinbix/angular-ui/theming'] = path.resolve(
     __dirname,
     'angular-ui/_theming'
-  );
-  config.resolve.alias['@sinbix/ngx-ui-material/theming'] = path.resolve(
-    __dirname,
-    'ngx-ui-material/_theming'
   );
 
   return config;
